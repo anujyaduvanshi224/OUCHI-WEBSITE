ff2b53523749b5c580df57b21e345c3ba343c777
Anuj Yaduvanshi <216862195+anujyaduvanshi224@users.noreply.github.com>
2026-03-26 11:56:08 +0530

    Create the Marquee Section

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Marquee = () => {
+  return (
+    <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+        <div className='text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap'>
+            <h1 className='text-[24vw] leading-none font-["Founders Grotesk X"] uppercase font-semibold pt-10 mb-12'>We are Ouchi</h1>
+            <h1 className='text-[24vw] leading-none font-["Founders Grotesk X"] uppercase font-semibold pt-10 mb-12'>We are Ouchi</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Marquee;
