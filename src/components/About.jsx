b08d7d5489c9f084d984feeb01c2aded93a3b462
Anuj Yaduvanshi <216862195+anujyaduvanshi224@users.noreply.github.com>
2026-03-26 13:05:27 +0530

    Create the About Section

---
+++ src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black'>
+            <h1 className='font-["Neue Montreal"] text-[4vw] leading-[4.5vw] tracking-tight'>
+                Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.
+            </h1>
+            <div className=' flex gap-5 w-full border-t pt-10 mt-20 border-[#a1b562]'>
+                <div className='w-1/2'>
+                    <h1 className='text-6xl '>Our approach: </h1>
+                    <button className='uppercase flex gap-10 items-center mt-10 px-10 py-6 bg-zinc-900 rounded-full text-white'>Read More
+                        <div className='w-2 h-2 bg-zinc-100 rounded-full'></div>
+                    </button>
+                </div>
+                <div className='w-1/2 h-[70vh] bg-[#b0c859] rounded-3xl'></div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,8 +4,8 @@ const Marquee = () => {
   return (
     <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
         <div className='text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap'>
-            <h1 className='text-[24vw] leading-none font-["Founders Grotesk X"] uppercase font-semibold pt-10 mb-12'>We are Ouchi</h1>
-            <h1 className='text-[24vw] leading-none font-["Founders Grotesk X"] uppercase font-semibold pt-10 mb-12'>We are Ouchi</h1>
+            <h1 className='text-[24vw] leading-none font-["Founders Grotesk X"] uppercase font-semibold pt-10 mb-[6vw]'>We are Ouchi</h1>
+            <h1 className='text-[24vw] leading-none font-["Founders Grotesk X"] uppercase font-semibold pt-10 mb-[6vw]'>We are Ouchi</h1>
         </div>
     </div>
   )
